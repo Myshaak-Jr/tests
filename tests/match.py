d31a256d64cc63ac340cb25db57e2a4ae3324bb0
Myshaak-Jr <80391448+Myshaak-Jr@users.noreply.github.com>
2021-10-22 15:01:15 +0200

    changed Tractor to Color

--- tests/match.py
+++ tests/match.py
@@ -1,25 +1,30 @@
-from typing import Tuple
+class Color:
+    r: int
+    g: int
+    b: int
 
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
 
-Color = Tuple[int, int, int]
+    __match_args__: tuple = ("r", "g", "b")
 
-class Tractor:
-    color: Color
-    def __init__(self, color: Color) -> None:
-        self.color = color
+    def __format__(self, format_spec: str) -> str:
+        return format_spec.replace("%r", f"{self.r}").replace("%g", f"{self.g}").replace("%b", f"{self.b}").replace("%x", f"#{self.r:0>2x}{self.g:0>2x}{self.b:0>2x}").replace("%X", f"#{self.r:0>2X}{self.g:0>2X}{self.b:0>2X}")
 
-
-def controlColor(tractor: Tractor) -> None:
-    match tractor:
-        case Tractor((0, 0, 0)):
-            print("Tractor is black.")
-        case Tractor((255, 255, 255)):
-            print("Tractor is white.")
-        case Tractor((r, g, b)) if r == g == b:
-            print("Tractor is gray.")
-        case Tractor((r, g, b)) if g == b == 0:
-            print("Tractor is red.")
-        case Tractor((r, g, b)) if r == b == 0:
-            print("Tractor is green.")
-        case Tractor((r, g, b)) if r == g == 0:
-            print("Tractor is blue.")
+def ctrl(color: Color) -> None:
+    match color:
+        case Color(0, 0, 0):
+            print("Color is black")
+        case Color(255, 255, 255):
+            print("Color is white")
+        case Color(r, g, b) if r == g == b:
+            print("Color is gray")
+        case Color(r, 0, 0):
+            print(f"Color is red ({r = :0>2x})")
+        case Color(0, g, 0):
+            print(f"Color is red ({g = :0>2x})")
+        case Color(0, 0, b):
+            print(f"Color is red ({b = :0>2x})")
+            
